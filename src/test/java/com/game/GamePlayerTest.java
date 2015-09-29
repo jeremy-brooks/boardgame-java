92868902f6c7884b590a6c215238e2112a0b913e
jeremyleebrooks <jeremybrooks321@gmail.com>
2015-09-29 14:54:35 +0100

    Refactored unit test classes

---
+++ src/test/java/com/game/GamePlayerTest.java
@@ -0,0 +1,46 @@
+package com.game;
+
+import com.game.GamePlayer;
+import cucumber.api.junit.Cucumber;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by jbrooks on 28/09/2015.
+ */
+
+@RunWith(Cucumber.class)
+public class GamePlayerTest {
+
+    GamePlayer player;
+
+    @Before
+    public void setUp(){
+        player = new GamePlayer();
+    }
+    @After
+    public void tearDown(){
+        player = null;
+    }
+
+    @Test
+    public void testSetPosition() throws Exception {
+        player.setPosition(0,0);
+        Assert.assertEquals(player.getColumn(), 0);
+        Assert.assertEquals(player.getRow(), 0);
+    }
+
+    @Test
+    public void has2Lives() {
+        Assert.assertEquals(2, player.getLives());
+    }
+
+    @Test
+    public void hitting1MineRemoves1Life(){
+        player.hitMine();
+        Assert.assertEquals(1, player.getLives());
+    }
+}
